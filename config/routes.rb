22609648de541a73d0a824283fce89a6a0593fc1
jackmccalley <jackmccalley@gmail.com>
2018-03-13 11:46:47 -0700

    API Endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/dogs' => 'dogs#index'
+  post '/dogs' => 'dogs#create'
 end
